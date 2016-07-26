657a7958238c8ffadd98df23f7cc11b52e51b269
Jeremy Shore <xboxmusicn3rd@gmail.com>
2016-07-26 16:07:46 -0500

    commenting out stetho not needed for release

--- crmsdk2015/src/main/java/com/microsoft/xrm/sdk/Client/ServiceProxy.java
+++ crmsdk2015/src/main/java/com/microsoft/xrm/sdk/Client/ServiceProxy.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.ArrayMap;
 
-import com.facebook.stetho.okhttp3.StethoInterceptor;
+//import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -52,7 +52,7 @@ public abstract class ServiceProxy {
     private OkHttpClient buildClient() {
         return new OkHttpClient.Builder()
             .addInterceptor(authHeader)
-            .addNetworkInterceptor(new StethoInterceptor())
+//            .addNetworkInterceptor(new StethoInterceptor())
             .build();
     }
 
